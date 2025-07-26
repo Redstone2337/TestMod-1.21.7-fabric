c4e0ceaad130ad949a1a4bde4f7b72e30c6cfa74
Redstone2337 <3356168312@qq.com>
2025-07-26 20:54:43 +0800

    更新 ModItems.java

--- src/main/java/net/redstone233/test/items/ModItems.java
+++ src/main/java/net/redstone233/test/items/ModItems.java
@@ -16,11 +16,15 @@ public class ModItems {
 
 private static final int CUSTOM_MAX_COUNT = SetValueCountCommand.getCustomMaxSize();
 
+public static final float ATTACK_DAMAGE = 10.0f;
+
+public static final float ATTACK_SPEED = 2.5f;
+
     public static final Item SILICON = register("silicon", new Item.Settings().maxCount(CUSTOM_MAX_COUNT));
     public static final Item RAW_SILICON = register("raw_silicon",new Item.Settings().maxCount(CUSTOM_MAX_COUNT));
     public static final Item SILICON_INGOT = register("silicon_ingot", new Item.Settings().maxCount(CUSTOM_MAX_COUNT));
     public static final Item FREEZE_SWORD = register("freeze_sword",
-            settings -> new FreezeSwordItem(ModToolMaterial.SILICON,10.5f, 2.5f,settings),
+            settings -> new FreezeSwordItem(ModToolMaterial.SILICON,ATTACK_DAMAGE, ATTACK_SPEED,settings),
             new Item.Settings().maxDamage(300000)
                     /*.attributeModifiers(FreezeSwordItem.createAttributeModifiers())*/
     );
