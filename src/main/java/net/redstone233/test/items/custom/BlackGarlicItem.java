7ca45b846e6dfa0c75b952a8be2decfe6bfc40a4
redstone233 <3356168312@qq.com>
2025-08-24 11:42:43 +0800

    Update Some Files

--- src/main/java/net/redstone233/test/items/custom/BlackGarlicItem.java
+++ src/main/java/net/redstone233/test/items/custom/BlackGarlicItem.java
@@ -12,7 +12,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 
 public class BlackGarlicItem extends Item {
